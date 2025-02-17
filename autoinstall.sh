48f3dbe949c3f9fc51c097bed74d6097e0c400f8
Sn0wBaall <sinuhe.nieves@gmail.com>
2025-02-17 14:28:20 -0600

    Bugs fixed

--- autoinstall.sh
+++ autoinstall.sh
@@ -30,20 +30,22 @@ if [ $(id -u) == 0 ];then
 
 else
 
+  echo -ne "${BgreenColour}[+]${endColour}${grayColour} Plese type your user${endColour}${turquoiseColour} --> ${endColour}" && read user
+
   echo -e "\n${BgreenColour}[+]${endColour}${grayColour} Installing all dependencies${endColour}"
 
   sudo apt install -y libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
 
   echo -e "\n${BgreenColour}[+]${endColour}${grayColour} Installing necessary programs${endColour}\n"
 
-  sudo apt install -y kitty neofetch rofi feh sxhkd bspwm i3lock-fancy flameshot tty-clock cmake polybar cargo; cargo install lsd bat
+  sudo apt install -y kitty rofi moreutils feh sxhkd bspwm i3lock-fancy flameshot tty-clock cmake polybar cargo; cargo install lsd bat
 
   echo -e "\n${BgreenColour}[+]${endColour}${grayColour} Cloning necessary repositories${endColour}\n"
 
   cd ~/Downloads
 
-  git clone https://baskerville/bspwm
-  git clone https://baskerville/sxhkd
+  git clone https://github.com/baskerville/bspwm
+  git clone https://github.com/baskerville/sxhkd
   git clone https://github.com/yshui/picom
   git clone https://github.com/adi1090x/rofi
   git clone https://github.com/lr-tech/rofi-themes-collection
@@ -53,32 +55,33 @@ else
 
   cd ~/Downloads/bspwm; make; sudo make install
   cd ~/Downloads/sxhkd; make; sudo make install
-  cd ~/Downloads/picom; sudo meson setup --buildtype=release build; ninja -C build; yes|ninja -C build install
+  cd ~/Downloads/picom; meson setup --buildtype=release build; ninja -C build; yes|ninja -C build install
   cd ~/Downloads/rofi; ./setup.sh
   cd ~/Downloads/rofi-themes-collection; mkdir -p ~/.local/share/rofi/themes/; cp themes/* ~/.local/share/rofi/themes/
   cd ~/Downloads/Wallpapers; ./setup.sh
   cd ~/.config; mkdir bin; cd bin; touch target
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
   cd $dir/rei
 
+  cp -r .fonts ~
   cp -r bspwm ~/.config
   cp -r lsd ~/.config
   cp -r polybar ~/.config
   cp -r kitty ~/.config
-  cp -r neofetch ~/.config
   cp -r picom ~/.config
   cp -r sxhkd ~/.config
   cp -r .p10k.zsh ~
   cp -r .zshrc ~
   sudo cp -r nvim /opt
 
+  cd ~; cat .zshrc | sed "s/user/$user/g" | sponge .zshrc
+  fc-cache -v
+
   cd ~/Downloads; rm -rf {bspwm,sxhkd,picom,rofi,rofi-themes-collection,Wallpapers}
 
   echo -e "\n${ByellowColour}[!] Restarting the system${endColour}\n"
-  sleep 2
+  sleep 3
   sudo reboot
 
 fi
-
-
-
